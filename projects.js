21f412c971ddd0dfdf60645cd8037e4b6ef53934
theanej007 <grojzdekanej@gmail.com>
2025-03-01 16:05:47 +0100

    added auto description to project listings

--- projects.js
+++ projects.js
@@ -3,15 +3,29 @@ async function fe() {
         const repo = await fetch("https://api.github.com/users/theanej007/repos").then(res => res.json());
         let eleDiv = [];
         let eleH2 = [];
+        let eleP = [];
+        let eleA = [];
         for (let i = 0; i < repo.length; i++) {
             if (repo[i].name !== "theanej007") {
                 eleDiv[i] = document.createElement("div");
                 eleDiv[i].className = "projectDiv";
                 eleDiv[i].id = `p${i}`;
                 document.body.append(eleDiv[i]);
+
                 eleH2[i] = document.createElement("H2");
-                eleH2[i].textContent = repo[i].name;
+                eleH2[i].id = `h${i}`;
                 document.getElementById(`p${i}`).append(eleH2[i]);
+
+                eleA[i] = document.createElement("a");
+                eleA[i].textContent = repo[i].name + "\n";
+                eleA[i].className = "specialA";
+                eleA[i].target = "_blank";
+                eleA[i].href = `https://github.com/theanej007/${repo[i].name}`;
+                document.getElementById(`h${i}`).appendChild(eleA[i]);
+
+                eleP[i] = document.createElement("p");
+                eleP[i].textContent = repo[i].description;
+                document.getElementById(`p${i}`).append(eleP[i]);
             }
         }
     }
