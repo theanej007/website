0be0188b358060df87305cf09f19e4d51bc5bd59
theanej007 <grojzdekanej@gmail.com>
2025-03-13 22:12:53 +0100

    more log in stuff

--- login.js
+++ login.js
@@ -2,9 +2,11 @@ async function login(){
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
     console.log(username+ " "+password);
+    let data = {"username": username, "password": password};
     let a = await fetch("https://anej.kak.si/api/test.php", {
-        method: "POST"
-        
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(data)
     }).then(res => res.json()).catch(err => console.error(err));
     console.log(a);
 }
