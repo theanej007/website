600d84e47107e8fe5c835388b30293845328d54d
theanej007 <grojzdekanej@gmail.com>
2025-03-13 17:35:22 +0100

    trying api

--- login.js
+++ login.js
@@ -1,4 +1,11 @@
-function login(){
-    console.log("HI");
+async function login(){
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    console.log(username+ " "+password);
+    let a = await fetch("https://anej.kak.si/api/test.php", {
+        method: "POST"
+        
+    }).then(res => res.json()).catch(err => console.error(err));
+    console.log(a);
 }
 document.getElementById("loginBtn").onclick = login;
