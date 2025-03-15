3351be5e1fe051bec48552a6d509678b156e2d55
theanej007 <grojzdekanej@gmail.com>
2025-03-15 01:10:20 +0100

    log in now working

--- log_info.js
+++ log_info.js
@@ -1,13 +1,13 @@
 async function aa(){
-    if(await fetch("https://anej.kak.si/api/info.php").then(res => res.json())){
+    if(await fetch("https://anej.kak.si/api/info.php", {credentials: "include"}).then(res => res.json())){
         let b = document.getElementById("account");
         b.textContent = "LOG OUT";
         b.href = window.location.href;
-        b.onclick = logout();
+        b.onclick = logout;
 }
 }
 aa();
 async function logout() {
 console.log("aa");
-await fetch("https://anej.kak.si/api/logout.php")
+await fetch("https://anej.kak.si/api/logout.php", {credentials: "include"})
 }
