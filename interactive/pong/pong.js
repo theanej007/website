d520928b6eb4030c438f679c240eb93a1b3be917
theanej007 <grojzdekanej@gmail.com>
2025-04-06 01:02:52 +0200

    polishing pong

--- interactive/pong/pong.js
+++ interactive/pong/pong.js
@@ -7,6 +7,12 @@ let c = canvas.getContext('2d');
 c.fillStyle = "white";
 c.font = "30px Bit";
 
+var hit = new Audio("hitHurt.wav");
+var score = new Audio("pickupCoin.wav");
+var select = new Audio("blipSelect.wav");
+var blip = new Audio("blip.wav");
+
+const sleep = ms => new Promise(r => setTimeout(r, ms));
 
 let x1 = 15;
 let x2 = 607;
@@ -31,7 +37,8 @@ function animation() {
     c.fillRect(x1, y1, 18, 100);
     c.fillRect(x2, y2, 18, 100);
     c.fillRect(bx, by, 18, 18);
-    
+
+    // PADDLE MOVEMENT
     if (move1u && y1 < 375) {
         y1 += 1.5;
     }
@@ -44,18 +51,23 @@ function animation() {
     if (move2d && y2 > 5) {
         y2 -= 1.5;
     }
-    
+
+    // BALL COLLISIONS
     if (by < 5) {
         bsy = -bsy;
+        hit.play();
     }
     if (by > 457) {
         bsy = -bsy;
+        hit.play();
     }
     if (bx > x2 - 17 && by > y2 - 18 && by < y2 + 100) {
         bsy = -bsy;
+        hit.play();
     }
     if (bx < x1 + 17 && by > y2 - 18 && by < y2 + 100) {
         bsy = -bsy;
+        hit.play();
     }
     if (bx > x2 - 18 && bx < x2 - 3 && by > y2 - 17 && by < y2 + 99) {
         bounce(1);
@@ -63,28 +75,31 @@ function animation() {
     if (bx < x1 + 18 && bx > x1 + 3 && by > y1 - 17 && by < y1 + 99) {
         bounce(0);
     }
-    
+
+    // POINT SYSTEM
     if (bx < -18) {
-        score1++;
-        bx = 311;
-        by = 231;
-        bsx = (Math.random() * 100 > 50) ? 0.6 : -0.6;
-        bsy = (Math.random() * 100 > 50) ? 0.6 : -0.6;
-    }
-    if (bx > 640) {
         score2++;
         bx = 311;
         by = 231;
         bsx = (Math.random() * 100 > 50) ? 0.6 : -0.6;
         bsy = (Math.random() * 100 > 50) ? 0.6 : -0.6;
+        score.play();
     }
-    
+    if (bx > 640) {
+        score1++;
+        bx = 311;
+        by = 231;
+        bsx = (Math.random() * 100 > 50) ? 0.6 : -0.6;
+        bsy = (Math.random() * 100 > 50) ? 0.6 : -0.6;
+        score.play();
+    }
+
     bx += bsx;
     by += bsy;
-    
+
     c.fillRect(0, 0, 640, 5);
     c.fillRect(0, 475, 640, 5);
-    
+
     c.fillText(score1, 230, 50);
     c.fillText(score2, 387, 50);
 }
@@ -101,13 +116,42 @@ function bounce(a) {
     }
     let raz = (((a == 1) ? y1 : y2) + 50) - (by + 9);
     bsy = -raz * 0.03;
+    hit.play();
 }
 
-window.addEventListener("click", () => {
-    start++;
-    if(start == 1)
-        animation();
+// STARTING MENU
+window.addEventListener("click", async (event) => {
+    if (event.target == canvas) {
+        var canvasPos = event.target.getBoundingClientRect();
+        var x = event.clientX - canvasPos.left;
+        var y = event.clientY - canvasPos.top;
+        if (x > 250 && x < 360 && y > 200 && y < 240) {
+            start++;
+            if (start == 1) {
+                select.play();
+                await startSeq();
+                animation();
+            }
+        }
+    }
 })
+
+async function startSeq() {
+    c.clearRect(0, 0, 640, 480);
+    c.fillText("3", 305, 230);
+    await sleep(1000);
+    select.play();
+    c.clearRect(0, 0, 640, 480);
+    c.fillText("2", 305, 230);
+    await sleep(1000);
+    select.play();
+    c.clearRect(0, 0, 640, 480);
+    c.fillText("1", 305, 230);
+    await sleep(1000);
+    blip.play();
+}
+
+// INPUT
 window.addEventListener("keydown", (event) => {
     if (event.key === "ArrowDown") {
         move1u = true;
